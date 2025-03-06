ba54e8f61ddbdc41ef950d5e400f396179b06a62
Britain Slack <Bs16971@ms.wasatch.edu>
2025-03-06 13:22:27 -0700

    continued shop

--- Assets/_Scripts/Shop/PlayButton.cs
+++ Assets/_Scripts/Shop/PlayButton.cs
@@ -6,12 +6,22 @@ using UnityEngine.SceneManagement;
 public class PlayButton : MonoBehaviour
 {
 
-   [SerializeField] private string _playGame = "Fishing";
-   [SerializeField] private string _shop = "Shop";
-   public void NewGameButton()
-   {
-      SceneManager.LoadScene(_playGame);
-      SceneManager.LoadScene(_shop);
-   }
-   
+    [SerializeField] private string _playGame = "Fishing";
+    [SerializeField] private string _shop = "Shop";
+    [SerializeField] private string _hook = "ShopHook";
+
+    public void NewGameButton()
+    {
+        SceneManager.LoadScene(_playGame);
+    }
+
+    public void ShopSystem()
+    {
+        SceneManager.LoadScene(_shop);
+    }
+
+    public void HookSystem()
+    {
+        SceneManager.LoadScene(_hook);
+    }
 }
